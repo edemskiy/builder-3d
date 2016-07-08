8f62697b84ad33f74b13fa4f6b65fba4d432e1b5
Edemskiy Alexander <qapple@ya.ru>
2016-07-08 15:50:55 +0300

    some changes for babylon.html and MyScene.js

--- MyScene.js
+++ MyScene.js
@@ -85,9 +85,6 @@ class Map {
       this.canvas = canvas;
       this.engine = new BABYLON.Engine(canvas, true);
       this.scene = new Scene(this.engine).getScene();
-      window.addEventListener("resize", () => {
-         this.engine.resize();
-      });
    }
    runRenderLoop() {
       this.engine.runRenderLoop(() => {
