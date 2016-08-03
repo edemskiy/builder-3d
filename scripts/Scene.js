bc39ca1bc84044c9a4615df025fff265e89bd6fa
edemskiy <qapple@ya.ru>
2016-08-03 15:01:09 +0300

    doors & windows fixes

--- scripts/Scene.js
+++ scripts/Scene.js
@@ -3,7 +3,7 @@ class Scene {
       let scene = new BABYLON.Scene(engine);
       scene.clearColor = new BABYLON.Color3(1, 1, 1);
       scene.collisionsEnabled = true;
-      scene.gravity = new BABYLON.Vector3(0, -1, 0); 
+      scene.gravity = new BABYLON.Vector3(0, -1, 0);
       this.scene = scene;
       
       let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(15, 5, -30), this.scene);
@@ -15,52 +15,60 @@ class Scene {
       this.camera = camera;
 
       /* Light */
-      let light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, -1), scene);
+      let light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, -1), this.scene);
       light1.intensity = .75;
 
-      let room1 = new TRoom(25, 40, 70, scene);
-      let room2 = new TRoom(25, 50, 30, scene);
-      let room3 = new TRoom(25, 30, 40, scene);
+      let room1 = new TRoom(25, 40, 70, this.scene);
+      let room2 = new TRoom(25, 50, 30, this.scene);
+      let room3 = new TRoom(25, 30, 40, this.scene);
 
       room2.setPosition(-45, 0, -20);
       room3.setPosition(-35, 0 , 15);
 
-      room1.getLeftWall().addDoor(10, 6, 52, scene);
+      room1.getLeftWall().addDoor(10, 6, 7, this.scene);
+      room1.getLeftWall().addDoor(10, 6, 55, this.scene);
       room2.getRightWall().remove();
 
-      room2.getFrontWall().addDoor(10, 6, 32, scene);
+      room2.getFrontWall().addDoor(10, 6, 32, this.scene);
       room3.getBackWall().remove();
 
-      room1.getLeftWall().addDoor(10, 6, 5, scene);
+      room1.getLeftWall().addDoor(10, 6, 50, this.scene);
       room3.getRightWall().remove();
 
+      room1.getRightWall().addWindow(4, 7, 10, 18, this.scene);
+      room1.getRightWall().addWindow(4, 4, 20, 18, this.scene);
+      room1.getRightWall().addWindow(4, 4, 30, 18, this.scene);
+      room1.getRightWall().addDoor(10, 6, 40, this.scene);
 
-
-      let secFloor = new TFloor(room1.getFloor().height/3, room1.getFloor().width/4, scene);
+      let secFloor = new TFloor(room1.getFloor().height/3, room1.getFloor().width/4, this.scene);
       secFloor.setPosition(-secFloor.width*1.5, room1.getBackWall().height/4, secFloor.height);
       this.secFloor = secFloor;
 
       /* Stairs */
       //Params for stairs: height, width, length, stairsNum, scene
       let stairs1 = new TStairs(secFloor.getPosition().y, secFloor.width, secFloor.height/2, Math.round(secFloor.getPosition().y*1.5), this.scene);
+      stairs1.setPosition(secFloor.getPosition().x, stairs1.stairHeight/2 + 0.25, secFloor.height/2 - stairs1.length + stairs1.stairLength/2);
+      stairs1.rotateY(0);
+
       let stairsMaterial = new BABYLON.StandardMaterial("stairsMaterial", this.scene);
       stairsMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
       stairs1.setMaterial(stairsMaterial);
-      stairs1.setPosition(secFloor.getPosition().x, stairs1.stairHeight/2 + 0.25, secFloor.height/2 - stairs1.length + stairs1.stairLength/2);
-      stairs1.rotateY(0);
+      
       this.stairs1 = stairs1;
 
-      let thirdFloor = new TFloor(room1.getFloor().height/2, room1.getFloor().width/4, scene);
+      let thirdFloor = new TFloor(room1.getFloor().height/2, room1.getFloor().width/4, this.scene);
       thirdFloor.setPosition(thirdFloor.width*1.5, room1.getBackWall().height*0.6, thirdFloor.height/2);
       this.thirdFloor = thirdFloor;
 
       let stairs2 = new TStairs(thirdFloor.getPosition().y - secFloor.getPosition().y, thirdFloor.width, 
          Math.abs(thirdFloor.getPosition().x - thirdFloor.width/2 - secFloor.getPosition().x - secFloor.width/2),
           Math.round( (thirdFloor.getPosition().y - secFloor.getPosition().y)*1.5 ), this.scene);
-      stairs2.setMaterial(stairsMaterial);
+
       stairs2.setPosition(secFloor.getPosition().x + secFloor.width/2 + stairs2.stairLength/2, secFloor.getPosition().y + stairs2.stairHeight/2 + 0.25,
        room1.getFrontWall().getPosition().z - stairs2.width/2);
+      
       stairs2.rotateY(Math.PI/2);
+      stairs2.setMaterial(stairsMaterial);
       this.stairs2 = stairs2;
    }
    getScene(){
--- scripts/TWall.js
+++ scripts/TWall.js
@@ -34,36 +34,54 @@ class TWall extends TRigid{
       if(this.meshArr.length === 1)
          this.getMesh(0).dispose();
    }
-   addDoor(height, width, from, scene){
-      if(this.meshArr.length === 1){  //if wall has no doors
-         this.setSize(this.height - height, this.width);
+   /* На одну стену можно добавить несколько окон, только если они одной высоты и находятся на одном уровне. */
+   addWindow(height, width, xPos, yPos, scene){
+      if(this.meshArr.length === 1){
          let currPos = this.getPosition();
-         this.setPosition(currPos.x, currPos.y + height/2, currPos.z);
-      
-         let leftWall = new TWall(height, from, scene);
+         
+         this.setSize(this.height - height - yPos, this.width);
+         this.setPosition(currPos.x, currPos.y + this.height/2 - (this.height - height - yPos)/2 , currPos.z);
+
+         let leftWall = new TWall(height, xPos, scene);
          leftWall.rotateY(this.rotation);
 
-         let rightWall = new TWall(height, this.width - from - width, scene);
+         let rightWall = new TWall(height, this.width - xPos - width, scene);
          rightWall.rotateY(this.rotation);
 
-         leftWall.setPosition(currPos.x - Math.cos(this.rotation) * (this.width/2 - from/2), 
-            currPos.y - this.height/2 + height/2,
-            currPos.z - Math.sin(this.rotation) * (this.width/2 - from/2));
+         leftWall.setPosition(currPos.x - Math.cos(this.rotation) * (this.width/2 - xPos/2), 
+            currPos.y - this.height/2 + height/2 + yPos,
+            currPos.z + Math.sin(this.rotation) * (this.width/2 - xPos/2));
 
-         rightWall.setPosition(leftWall.getPosition().x + (from/2 + width + rightWall.width/2) * Math.cos(this.rotation),
-            currPos.y - this.height/2 + height/2,
-            leftWall.getPosition().z + (from/2 + width + rightWall.width/2) * Math.sin(this.rotation));
+         rightWall.setPosition(leftWall.getPosition().x + (xPos/2 + width + rightWall.width/2) * Math.cos(this.rotation),
+            currPos.y - this.height/2 + height/2 + yPos,
+            leftWall.getPosition().z - (xPos/2 + width + rightWall.width/2) * Math.sin(this.rotation));
 
          leftWall.setMaterial(this.material);
          rightWall.setMaterial(this.material);
 
-         this.meshArr.push(leftWall);
-         this.meshArr.push(rightWall);
-      }
-      if(from + width < this.getMesh(1).width)
-         this.getMesh(1).addDoor(height, width, from, scene);
+         this.addMesh(leftWall);
+         this.addMesh(rightWall);
 
-      if(from > this.width - this.getMesh(2).width)
-         this.getMesh(2).addDoor(height, width, from - (this.width - this.getMesh(2).width), scene);
+         if(yPos !== 0){
+            let bottomWall = new TWall(yPos, this.width, scene);
+            bottomWall.rotateY(this.rotation);
+            bottomWall.setPosition(currPos.x, currPos.y - this.height/2 + yPos/2, currPos.z);
+            bottomWall.setMaterial(this.material);
+            this.addMesh(bottomWall);
+         }
+      }
+      if(xPos + width < this.getMesh(1).width && height <= this.getMesh(1).height)
+         this.getMesh(1).addWindow(height + 1, width, xPos, 0, scene);
+
+      if(xPos > this.width - this.getMesh(2).width && height <= this.getMesh(2).height)
+         this.getMesh(2).addWindow(height + 1, width, xPos - (this.width - this.getMesh(2).width), 0, scene);
+
+      if(this.getMesh(3) !== undefined && height + yPos < this.getMesh(3).height)
+         this.getMesh(3).addWindow(height, width, xPos, 0, scene);
+   }
+   /* На стену с окнами можно добавить двери, только если они одной высоты и не выше нижней рамки окон.
+      Двери добавляются только после окон, но не наоборот */
+   addDoor(height, width, xPos, scene){
+      this.addWindow(height, width, xPos, 0, scene);
    }
 };
